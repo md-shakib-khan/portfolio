14bdf6eaf6521ea1b3c94a9908f5fa113f3a2eed
professor-2024 <sk009900990099101@gmail.com>
2025-01-31 14:44:34 +0600

    feature: added not found page

---
+++ web/app/not-found.tsx
@@ -0,0 +1,10 @@
+import { NotFoundImage } from '@/components/NotFound/NotFoundImage'
+import React from 'react'
+
+export default function _notfound() {
+  return (
+    <div className='flex items-center w-screen h-screen'>
+      <NotFoundImage />
+    </div>
+  )
+}
