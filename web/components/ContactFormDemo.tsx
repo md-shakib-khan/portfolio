e34be80a2334018cfce6a8a96e90e796f0709bbd
md-shakib-khan <mdshakibkhan.dev@gmail.com>
2025-02-17 20:56:34 +0600

    added theme system in contact page

--- web/components/ContactFormDemo.tsx
+++ web/components/ContactFormDemo.tsx
@@ -1,48 +1,84 @@
 "use client";
-import React from "react";
-import { Label } from "./ui/label";
-import { Input } from "./ui/input";
+import { useGlobalContext } from "@/context/GlobalContextProvider";
 import { cn } from "@/lib/utils";
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandOnlyfans,
-} from "@tabler/icons-react";
 import { Textarea } from "@mantine/core";
+import React from "react";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 export function ContactFormDemo() {
+  const { theme } = useGlobalContext();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted");
   };
   return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black lg:mt-20">
-      <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+    <div
+      className={`"" ${
+        theme === "dark"
+          ? "bg-black max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input lg:mt-20"
+          : "bg-white max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input lg:mt-20"
+      }`}
+    >
+      <h2
+        className={`"font-bold text-xl " ${
+          theme === "dark" ? "text-neutral-200" : "text-neutral-800 "
+        }`}
+      >
         Get In Touch
       </h2>
-      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-        Hey There, If you want to get in touch with me, Please feel free fill this form .
+      <p
+        className={`" text-sm max-w-sm mt-2" ${
+          theme === "dark" ? "text-neutral-300" : "text-neutral-600"
+        }`}
+      >
+        Hey There, If you want to get in touch with me, Please feel free fill
+        this form .
       </p>
 
       <form className="my-8" onSubmit={handleSubmit}>
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
             <Label htmlFor="firstname">First name</Label>
-            <Input id="firstname" placeholder="Tyler" type="text" />
+            <Input
+              style={{ backgroundColor: theme === "dark" ? "#000" : "#fff" }}
+              id="firstname"
+              placeholder="Tyler"
+              type="text"
+            />
           </LabelInputContainer>
           <LabelInputContainer>
             <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" placeholder="Durden" type="text" />
+            <Input
+              style={{ backgroundColor: theme === "dark" ? "#000" : "#fff" }}
+              id="lastname"
+              placeholder="Durden"
+              type="text"
+            />
           </LabelInputContainer>
         </div>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+          <Input
+            style={{ backgroundColor: theme === "dark" ? "#000" : "#fff" }}
+            id="email"
+            placeholder="projectmayhem@fc.com"
+            type="email"
+          />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="message">Message</Label>
           {/* <Input id="message" placeholder="Enter Your Message" type="text"/> */}
-          <Textarea id="message" placeholder="Enter Your Message" />
+          <Textarea
+            id="message"
+            placeholder="Enter Your Message"
+            styles={{
+              input: {
+                backgroundColor: theme === "dark" ? "#000" : "#fff",
+                color: theme === "dark" ? "#fff" : "#000",
+              },
+            }}
+          />
         </LabelInputContainer>
 
         <button
